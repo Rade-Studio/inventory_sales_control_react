86420a68593022d7f6801fa16dcde3306d8023ae
root <root@WIN-27UBQ42L5S9>
2023-10-01 11:41:18 -0500

    implementando configuraciones de rutas generales

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "Utilities/components/navbar";
+import "./index.css";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>,
 )
 
